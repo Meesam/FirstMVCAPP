9814a91db89346dac082a4dd8d6d0006a2878a76
Meesam Zaidi <meesam.engineer@gmail.com>
2025-02-20 21:09:41 +0530

    Add project files.

---
+++ FirstMVCAPP/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using FirstMVCAPP.Data;
+using FirstMVCAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVCAPP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+    }
+}
